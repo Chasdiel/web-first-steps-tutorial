92222d6ca526ecdb011afc8af1044aa8e0576a38
chasdiel <chasdiel@gmail.com>
2019-01-28 10:08:33 +0100

    Wprowadzenie przycisku hide/unhide do Tab 2

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,20 @@ function hideTab(tabClassName) {
     var element = document.getElementsByClassName(tabClassName);
     element[0].style.display = 'none';
 }
+
+// hide / unhide
+
+var hideUnhideBtn = document.getElementById('hideBtn');
+var isVisible = true;
+var contentPar = document.querySelector('.tab2 p');
+var temp = contentPar.textContent;
+
+hideUnhideBtn.addEventListener('click', function(){
+    if (isVisible){
+        contentPar.textContent = '';
+        isVisible = false;
+    } else {
+        contentPar.textContent = temp;
+        isVisible = true;
+    }
+});
